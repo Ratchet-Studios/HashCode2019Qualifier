4ea8a77e3ff1931ebecea7a9d67ae9718b37263e
beyarkay <boydrkane@gmail.com>
2019-02-27 22:12:32 +0200

    Added TODO messages to the template to aid in making changes

--- Main.py
+++ Main.py
@@ -7,23 +7,29 @@ class Data:
         # Create a 2D array, split first by each line, then by spaces foreach line
         elements = []
         for line in lines:
+            # TODO: The line below assumes all entries are integers. Is this true?
             elements.append(int(entry) for entry in line.split(" "))  # convert the strings to integers
 
-        # Just guessing the format of the data after this...
+        # TODO rename the variables as appropriate
         self.rows = elements[0][0]
         self.cols = elements[0][1]
         self.var1 = elements[0][2]
         self.var2 = elements[0][3]
         self.var3 = elements[0][4]
 
-        # Often they'll give a list of things, with each element on a differnt line.
-        # Parse a newline-separated list, starting from int:origin until var1
         origin = 1
         self.arr1 = []
         for index in range(origin, origin + self.var1):
+        # TODO restructure the line below as appropriate
             self.arr1.append(elements[index])
         origin += self.var1
 
         self.arr2 = []
         for index in range(origin, origin + self.var2):
+            # TODO restructure the line below as appropriate
             self.arr2.append(elements[index])
+
+        # TODO Sort the arrays on proper variables
+        self.arr1.sort(key=lambda entry: entry[0])
+        self.arr2.sort(key=lambda entry: entry[0])
+
