aafd51e9f6b98e8a638f27c4970020bf010a4371
beyarkay <boydrkane@gmail.com>
2019-02-28 22:02:24 +0200

    Fixed stu's bug

---
+++ main_boyd.py
@@ -0,0 +1,28 @@
+import InputOutput
+
+files = ["a_example.txt",
+         "b_lovely_landscape.txt",
+         "c_memorable_moments.txt",
+         "d_pet_pictures.txt",
+         "e_shiny_selfies.txt"]
+data = InputOutput.Data("input_data/" + files[0])
+"""
+data.photos[photo_id][0] - photo id
+data.photos[photo_id][1] - 'h' or 'v'
+data.photos[photo_id][2] - number of tags of this photo
+data.photos[photo_id][3] - a list, all the tags of that photo
+
+data.num_photos - the number of photos in the collection in total
+
+data.tags[my_tag][0] - unique id for each tag
+data.tags[my_tag][1] - counter for how often that tag occurs
+"""
+output = InputOutput.Output()
+"""
+output.add_slide(index_of_slide_in_show, [photo_id_0, optional_id_1])
+output.write(my_output_file_name)
+"""
+
+# Boyd's solution
+def get_score(slide1, slide2):
+
