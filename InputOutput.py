b8f56a667176e3673b7bfad0ba35826658372454
beyarkay <boydrkane@gmail.com>
2019-02-28 20:06:24 +0200

    Finished initial data class, NOT tested just yet

--- InputOutput.py
+++ InputOutput.py
@@ -7,31 +7,18 @@ class Data:
         # Create a 2D array, split first by each line, then by spaces foreach line
         elements = []
         for line in lines:
-            # TODO: The line below assumes all entries are integers. Is this true?
-            elements.append(int(entry) for entry in line.split(" "))  # convert the strings to integers
+            elements.append(entry for entry in line.split(" "))  # convert the strings to integers
 
-        # TODO rename the variables as appropriate
-        self.num_photos = elements[0][0]
-        self.cols = elements[0][1]
-        self.var1 = elements[0][2]
-        self.var2 = elements[0][3]
-        self.var3 = elements[0][4]
+        self.num_photos = int(elements[0][0])
 
         origin = 1
-        self.arr1 = []
-        for index in range(origin, origin + self.var1):
-        # TODO restructure the line below as appropriate
-            self.arr1.append(elements[index])
-        origin += self.var1
-
-        self.arr2 = []
-        for index in range(origin, origin + self.var2):
-            # TODO restructure the line below as appropriate
-            self.arr2.append(elements[index])
-
-        # TODO Sort the arrays on proper variables
-        self.arr1.sort(key=lambda entry: entry[0])
-        self.arr2.sort(key=lambda entry: entry[0])
+        self.photos = []
+        for index in range(origin, origin + self.num_photos):
+            self.photos.append([
+                elements[index][0].lower(),  # [0] = Horizontal or vertical?, lowercase 'h' or 'v'
+                int(elements[index][1]),  # [1] = number of tags, integer
+                elements[index][2:]  # [2] = all the tags of that photo, array with lowercase strings
+            ])
 
 
 class Output:
