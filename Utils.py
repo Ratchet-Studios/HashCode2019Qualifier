e8f9a5497a26103d2ffb78c0e005f49a0cbd3f57
beyarkay <boydrkane@gmail.com>
2019-02-28 23:16:14 +0200

    Updated the method for converting vertical photos into files

--- Utils.py
+++ Utils.py
@@ -10,7 +10,7 @@ def calculate_score(set_of_tags_1, set_of_tags_2):
     return min(one_and_two, one_not_two, two_not_one)
 
 
-def create_slides(photos):
+def create_slides(photos, vertical_photos_method=0):
     slides = []  # [([photo ids], {tag ids})] each slide is a tuple ([photo ids], {tag ids})
     vertical_photos = []
 
@@ -20,16 +20,36 @@ def create_slides(photos):
         else:
             vertical_photos.append(photo)
 
-    # vertical photo matchmaking
-    index = 0
-    while index + 1 < len(vertical_photos):
-        slides.append(
-            (
-                [vertical_photos[index][0], vertical_photos[index + 1][0]],  # the photo ids in this slide
-                set(vertical_photos[index][4]).union(set(vertical_photos[index + 1][4]))  # the tags in this slide
+    if vertical_photos_method == 1:
+
+        # vertical photo matchmaking
+        index = 0
+        while index + 1 < len(vertical_photos):
+            slides.append(
+                (
+                    [vertical_photos[index][0], vertical_photos[index + 1][0]],  # the photo ids in this slide
+                    set(vertical_photos[index][4]).union(set(vertical_photos[index + 1][4]))  # the tags in this slide
+                )
             )
-        )
+            index += 1
+    else:
+        used_photos = []
+        for i in range(len(vertical_photos) - 1):
+            if vertical_photos[i] not in used_photos:
+                low_score = sys.maxsize
+                low_index = -1
+                for j in range(i + 1, len(vertical_photos)):
+                    if vertical_photos[j] not in used_photos:
 
-        index += 1
+                        score = len(vertical_photos[i][3] & vertical_photos[j][3])
+                        if score < low_score:
+                            low_score = score
+                            low_index = j
 
+                slides.append((
+                    [vertical_photos[i][0], vertical_photos[low_index][0]],
+                    vertical_photos[i][3] | vertical_photos[low_index][3]
+                ))
+                used_photos.append(vertical_photos[i])
+                used_photos.append(vertical_photos[low_index])
     return slides
